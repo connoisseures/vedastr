f3948b9e1ff802336308fa05bb71310f87d6dff9
connoisseures <connoisseures@gmail.com>
2021-10-23 12:58:30 -0700

    colab

---
+++ sh/colab_export_satrn.sh
@@ -0,0 +1,5 @@
+# export model to onnx using GPU with gpu_id 0
+PTH="../drive/MyDrive/pytorch_pth/small_satrn.pth"
+OUTFILE="satrn.onnx"
+CUDA_VISIBLE_DEVICES="0" python tools/torch2onnx.py configs/small_satrn.py $PTH $OUTFILE --dummy_input_shape "3,32,100" --dynamic_shape
+
---
+++ sh/colab_inf_pth_satrn.sh
@@ -0,0 +1,4 @@
+# inference using GPUs with gpu_id 0
+python tools/inference.py configs/small_satrn.py "../drive/MyDrive/pytorch_pth/small_satrn.pth" "./test_images/ssn00001_sub12.png" "0" 
+python tools/inference.py configs/small_satrn.py "../drive/MyDrive/pytorch_pth/small_satrn.pth" "./test_images/ssn00001_sub13.png" "0" 
+
